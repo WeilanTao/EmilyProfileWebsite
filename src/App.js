90e081046246243a7589e93ab2036c94568b7714
WeilanTao <twlemily@hotmail.com>
2022-02-15 11:56:26 -0500

    enable react-canvas-nest

--- src/App.js
+++ src/App.js
@@ -9,10 +9,22 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
 import Header from "./components/header";
+import ReactCanvasNest from "react-canvas-nest";
 
 function App() {
   return (
-    <>
+    <div height="1440" width="3060">
+      <ReactCanvasNest
+        className="canvasNest"
+        config={{
+          pointColor: " 255, 255, 255 ",
+          pointR: "0",
+          lineColor: "255,204,204",
+          lineWidth: "3",
+          count: "100",
+        }}
+        style={{ zIndex: -1 }}
+      />
       <Header />
       <NavBar />
 
@@ -28,7 +40,7 @@ function App() {
       </div>
 
       <Footer />
-    </>
+    </div>
   );
 }
 
