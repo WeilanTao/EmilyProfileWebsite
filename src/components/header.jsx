e9a91920bfdd078e4b8c2adac71fb9647444f425
WeilanTao <twlemily@hotmail.com>
2022-02-16 05:09:00 -0500

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import "../style/header.scss";
+
 const Header = () => {
   return (
     <>
-      <h1>Emily Tao</h1>
+      <h1 className="header">Emily Weilan Tao</h1>
     </>
   );
 };
