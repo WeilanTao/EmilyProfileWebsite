c7d06225e71532cceba2ecc73f8a845a069eb5b8
WeilanTao <twlemily@hotmail.com>
2022-03-08 21:07:52 -0500

    404 page issue v1

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <>
       <div className="notfoundPage">
-        <h1>oops...You've reached Not Found</h1>
+        <p>oops...You've reached Not Found</p>
       </div>
     </>
   );
